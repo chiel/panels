ca1bc2533a97062af2e8ef3a6c365170a572a372
Chiel Kunkels <chiel@kunkels.me>
2024-03-31 00:51:05 +0100

    feat: adjust widths on resize

--- src/common/panels/hooks/usePanels.ts
+++ src/common/panels/hooks/usePanels.ts
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 import { PanelsConfig } from '../types';
 import computeDefaultWidths from '../utils/computeDefaultWidths';
@@ -8,14 +8,24 @@ export default function usePanels(
 	availableWidth: number,
 	panelsConfig: PanelsConfig,
 ) {
+	const curAvailableWidth = useRef(availableWidth);
+
 	const finalPanelsConfig = useMemo(
 		() => finaliseConfig(availableWidth, panelsConfig),
 		[availableWidth, panelsConfig],
 	);
 
-	const [widths] = useState(() =>
+	const [widths, setWidths] = useState(() =>
 		computeDefaultWidths(availableWidth, finalPanelsConfig),
 	);
+
+	useEffect(() => {
+		if (availableWidth !== curAvailableWidth.current) {
+			const newWidths = computeDefaultWidths(availableWidth, finalPanelsConfig);
+			setWidths(newWidths);
+			curAvailableWidth.current = availableWidth;
+		}
+	}, [availableWidth, finalPanelsConfig]);
 
 	return useMemo(() => ({ widths }), [widths]);
 }
